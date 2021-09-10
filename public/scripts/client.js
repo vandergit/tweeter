2e9caa39a9cf0c2d6462b32e429dc0d77b46e4c1
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-09-09 21:36:13 -0700

    Submit Project for Evaluation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,84 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Like $(document).ready(), but lighter
+$(() => {
+  const loadTweets = () => {
+    // Promise type call with Ajax
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+      .then((response) => {
+        renderTweets(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  loadTweets();
+  
+  const renderTweets = function(tweets) {
+    const $tweetContainer = $('section.tweet-container');
+    $tweetContainer.empty();
+
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetContainer.prepend($tweet);
+    }
+
+    return $tweetContainer;
+  };
+
+  const createTweetElement = (tweet) => {
+    // Instead of calling $().text operations, the article element is shaped by the HTML container in index.html
+    const $article = `<article class="tweet" >
+                      <header>
+                        <div>
+                          <img src=${tweet.user.avatars}>
+                          <p>${tweet.user.name}</p>
+                        </div>
+                        <p>${tweet.user.handle}</p>
+                      </header>
+                      <p>
+                      ${tweet.content.text}
+                      </p>
+                      <footer>
+                        <p>${timeago.format(tweet.created_at)}</p>
+                        <p class="icon-hover">
+                          <i class="fas fa-flag"></i>
+                          <i class="fas fa-retweet"></i>
+                          <i class="fas fa-heart"></i>
+                        </p>
+                      </footer>
+                    </article>`;
+    return $article;
+  };
+
+  $('.tweet-form').on(('submit'), function(event) {
+    const $currentTweet = $('#tweet-text').val();
+    const messageIcon = `<i class="fas fa-exclamation-triangle"></i>`;
+
+    event.preventDefault();
+    
+    if ($currentTweet === null || $currentTweet === '') {
+      // alert('Tweet message is empty or not valid');
+      $("#error-message").html(`${messageIcon} Tweet message is empty or not valid ${messageIcon}`).slideDown(500, function() {
+        $(this).fadeOut(5000);
+      });
+      return;
+    }
+    
+    if ($currentTweet.length > 140) {
+      // alert('Tweet message longer than allowed');
+      $("#error-message").html(`${messageIcon} Tweet message longer than allowed ${messageIcon}`).slideDown(500, function() {
+        $(this).fadeOut(5000);
+      });
+      return;
+    }
+
+    const serializedData = $(this).serialize();
+
+    $.post('/tweets', serializedData, () => {
+      loadTweets();
+    });
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,14 @@
 $(document).ready(() => {
-  const $tweet = $('#tweet-text');
-  
-  $('#tweet-text').on('keyup', function(event) {
+  $('#tweet-text').on('keyup', function() {
     const $remainChar = $('#chars-counter');
     let $charInput = $(this); // #tweet-text DOM element is the source of our event.
     let textLength  = $charInput.val().length;
     let diff = 140 - textLength;
 
-    if (diff < 0 ) {
+    if (diff < 0) {
       $($remainChar).html(diff).addClass('off-limit-counter');
     } else {
       $($remainChar).html(diff).removeClass('off-limit-counter');
     }
-  })
-})
+  });
+});
